6db7bfd64903960e7f0396d250a0a8036aaf6795
mldn <mldnqa@163.com>
2019-04-15 23:19:39 +0800

    ggg

--- hello3.java
+++ hello3.java
@@ -2,6 +2,9 @@ public class E
 {
      public static void main(String[] args){
 	     System.out.println("hello java");
+		  System.out.println("helloss java");
+		  System.out.println("helloss java");
+		   System.out.println("helloss java");
 	 }
 
 }
--- hello4.java
+++ hello4.java
@@ -2,8 +2,6 @@ public class E
 {
      public static void main(String[] args){
 	     System.out.println("hello java");
-		  System.out.println("hello git");
-		   System.out.println("hello git");
 	 }
 
 }
