bc833d3eb57934f7f4dc2901345d37abbc115f2a
mldn <mldnqa@163.com>
2019-04-16 22:31:02 +0800

    delete all

--- hello.java
+++
@@ -1,9 +0,0 @@
-public class E
-{
-     public static void main(String[] args){
-	     System.out.println("hello java");
-		  System.out.println("hello git");
-		   System.out.println("hello git");
-	 }
-
-}
